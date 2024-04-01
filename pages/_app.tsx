d852447ecec14d13ee2dd6bd2bd1e3df1ee97589
yjshi <495054021@qq.com>
2024-04-01 20:10:46 +0800

    add linea sepolia

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,18 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider, coinbaseWallet, embeddedWallet, metamaskWallet, rainbowWallet, smartWallet, trustWallet, walletConnect } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import { defineChain } from "thirdweb/chains";
+import { lineaSepolia } from "./linea-sepolia-chain"
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 // const activeChain = "sepolia";
 
-const activeChain = defineChain({
-  id: 59141,
-  rpc: "https://rpc.sepolia.linea.build",
-  name: "Linea Sepolia",
-  nativeCurrency: {
-    name: "Linea ETH",
-  },
-  testnet: true,
-  blockExplorers: [{
-    name: "sepolia lineascan",
-    url: "https://sepolia.lineascan.build/",
-  }],
-});
-
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
+      activeChain={lineaSepolia}
       //  引入钱包组件
       supportedWallets={[
         metamaskWallet(),
