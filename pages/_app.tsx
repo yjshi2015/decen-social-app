830c9ab515e4d6eef931c25e93f7b199e70c5d85
yjshi <495054021@qq.com>
2024-04-01 16:54:04 +0800

    add depency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,27 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider, coinbaseWallet, embeddedWallet, metamaskWallet, rainbowWallet, smartWallet, trustWallet, walletConnect } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import { defineChain } from "thirdweb/chains";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "sepolia";
+// const activeChain = "sepolia";
+
+const activeChain = defineChain({
+  id: 59141,
+  rpc: "https://rpc.sepolia.linea.build",
+  name: "Linea Sepolia",
+  nativeCurrency: {
+    name: "Linea ETH",
+  },
+  testnet: true,
+  blockExplorers: [{
+    name: "sepolia lineascan",
+    url: "https://sepolia.lineascan.build/",
+  }],
+});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
